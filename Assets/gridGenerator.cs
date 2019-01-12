44c37892909b1745cb15729b5acbdb3598f5e50f
unknown <jimmyhwrtn82@gmail.com>
2019-01-12 14:42:13 -0500

    Added functionality for visualizing grid differently in and out of combat and for finding new characters in area around player and generating grid to encompass their locations.

--- Assets/gridGenerator.cs
+++ Assets/gridGenerator.cs
@@ -4,52 +4,59 @@ using System.Linq;
 
 public class gridGenerator : MonoBehaviour
 {
-    public int size = 1;
+    public int size = 4;
     public int minX = -25;
     public int maxX = 25;
     public int minZ = -25;
     public int maxZ = 25;
+    public int distance = 20;
     public GameObject player;
     public List<GameObject> characters;
-    public Vector3[] charLocations;
-    Collider[] results;
+    public Collider[] results = new Collider[20];
 
-
-    void Start()
+    public void Start()
     {
         InvokeRepeating("locateGrid", 0, 1);
-        characters[0] = player;
+        characters.Add(player);
     }
 
     //TODO: make a maximum distance from the player that this can go to avoid infinite loops
     //TODO: could remove checks inside of known area
     void locateGrid()
     {
+        this.transform.position = player.transform.position;
         //check a box of 3 squares around each character, add characters in those areas to the list of
         //current characters
-        Vector3 boxSize = new Vector3(size * 3, size * 3, size * 3);
         bool searching = true;
         while (searching)
         {
             foreach (GameObject character in characters)
             {
+                Vector3 boxSize = new Vector3(size * distance, size * distance, size * distance);
                 Physics.OverlapBoxNonAlloc(transform.position, boxSize, results);
-                if (results.Length != 0)
+                Debug.Log(results);
+                foreach (Collider newChar in results)
                 {
-                    foreach (Collider newChar in results)
-                    {
-                        characters.Add(newChar.gameObject);
-                    }
-                    //need to re-search the list if new chars added
-                    break;
+                    Debug.Log(newChar.gameObject);
+                    characters.Add(newChar.gameObject);
                 }
+                //need to re-search the list if new chars added
+                break;
             }
             //if finished searching all chars with no new ones, end check
             searching = false;
         }
-        Debug.Log("balls");
-        //charLocations. (player.transform.position);
-        this.transform.position = charLocations[1];
+        foreach (GameObject item in characters)
+        {
+            if (Mathf.Abs(player.transform.position.x - item.transform.position.x) > maxX)
+            {
+                maxX = Mathf.RoundToInt(Mathf.Abs(player.transform.position.x - item.transform.position.x));
+            }
+            if (Mathf.Abs(player.transform.position.z - item.transform.position.z) > maxZ)
+            {
+                maxZ = Mathf.RoundToInt(Mathf.Abs(player.transform.position.z - item.transform.position.z));
+            }
+        }
     }
 
     public Vector3 GetNearestPointOnGrid(Vector3 position)
@@ -72,22 +79,23 @@ public class gridGenerator : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
-        for (float x = minX; x < maxX; x += size)
+        Gizmos.color = Color.red;
+        Vector3 boxSize = new Vector3(size * distance, size * distance, size * distance);
+        for (float x = -1 * maxX - 3 * size; x < maxX + 3 * size; x += size)
         {
-            for (float z = minZ; z < maxZ; z += size)
+            for (float z = -1 * maxZ - 3 * size; z < maxZ + 3 * size; z += size)
             {
                 var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
                 Gizmos.DrawSphere(point, 0.3f);
             }
 
         }
+        Gizmos.DrawCube(transform.position, boxSize);
     }
 
 
 
     void Update()
     {
-
     }
 }
