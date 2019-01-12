e9341ce8b2fbdf713e19121071226720fe71e294
unknown <jimmyhwrtn82@gmail.com>
2019-01-12 16:51:32 -0500

    Set up player movement via InControl This includes: - Lerp using x and y analog axes - camera Lerp to follow player via script

---
+++ Assets/LerpFollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpFollowPlayer : MonoBehaviour
+{
+
+    public float interpVelocity = 0.05f;
+    public GameObject target;
+    public Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, interpVelocity);
+        }
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController mover;
+
+    void Start()
+    {
+        //pull charcontroller from parent object
+        mover = GetComponentInParent(typeof(CharacterController)) as CharacterController;
+        //usually use charcontroller for collisions but I just use it
+        //because it is easier to move things around via simpleMove()
+        mover.detectCollisions = false;
+    }
+
+    void Update()
+    {
+        //pick active controller from InControl
+        var inputDevice = InputManager.ActiveDevice;
+        //set up floats from left axis of controller
+        float xMove = -1000.0f * Time.deltaTime * inputDevice.LeftStick.X;
+        float zMove = -1000.0f * Time.deltaTime * inputDevice.LeftStick.Y;
+        //use Lerp to interpolate movement for smoothness
+        Vector3 fullMove = Vector3.Lerp(mover.velocity, new Vector3(xMove, 0, zMove), 0.1f);
+        mover.SimpleMove(fullMove);
+    }
+}
